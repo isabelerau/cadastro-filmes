fbc8350b69568ef69f41a4d78a57102552d5e55f
Isabele Rau <bebel.candido@gmail.com>
2021-08-09 00:20:32 -0300

    Adição do arquivo de constants, configuração de task e env

---
+++ src/util/constantes.js
@@ -0,0 +1,82 @@
+const FILME_INICIAL = {
+  titulo: "",
+  subtitulo: "",
+  diretor: "",
+};
+
+const filmes = [
+  {
+    id: 1,
+    titulo: "The Green Mile",
+    subtitulo: "",
+    diretor: "Frank Darabont",
+    elenco: [
+      {
+        ator: "Tom Hanks",
+        personagem: "Paul Edgecomb",
+        papel: "Protagonista",
+      },
+      {
+        ator: "Michael Clarke Duncan",
+        personagem: "John Coffey",
+        papel: "Protagonista",
+      },
+    ],
+  },
+  {
+    id: 2,
+    titulo: "Intouchables",
+    subtitulo: "",
+    diretor: "Olivier Nakach e Éric Toledano",
+    elenco: [
+      {
+        ator: "François Cluzet",
+        personagem: "Philippe",
+        papel: "Protagonista",
+      },
+      {
+        ator: "Omar Sy",
+        personagem: "Driss",
+        papel: "Protagonista",
+      },
+    ],
+  },
+  {
+    id: 3,
+    titulo: "The Pursuit of Happyness",
+    subtitulo: "",
+    diretor: "Gabriele Muccino",
+    elenco: [
+      {
+        ator: "Will Smith",
+        personagem: "Chris Gardner",
+        papel: "Protagonista",
+      },
+      {
+        ator: "Jaden Smith",
+        personagem: "Christopher",
+        papel: "Protagonista",
+      },
+    ],
+  },
+  {
+    id: 4,
+    titulo: "Forrest Gump",
+    subtitulo: "",
+    diretor: "Robert Zemeckis",
+    elenco: [
+      {
+        ator: "Tom Hanks",
+        personagem: "Forrest Gump",
+        papel: "Protagonista",
+      },
+      {
+        ator: "Robin Wright",
+        personagem: "Jenny Curran",
+        papel: "Protagonista",
+      },
+    ],
+  },
+];
+
+export { filmes, FILME_INICIAL };
