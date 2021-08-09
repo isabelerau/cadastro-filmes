38679b0d175ddc11928f81786f411354b03956d2
Isabele Rau <bebel.candido@gmail.com>
2021-08-09 00:16:52 -0300

    Criação do projeto react

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# cadastro-filmes
+# Cadastro de filmes
+
+Aplicação onde você poderá visualizar filmes já cadastrados, alterar alguma informação destes, adicionar outros ou ainda excluí-los;
+Resultado de um treinamento de React oferecido pela Softplan e ministrado pelo meu grande colega Leonardo Biazus (https://github.com/lbiazus), em união com os meus conhecimentos.
+A aplicação foi desenvolvida em React e como o foco era este, foi utilizado o MirageJs para simular uma API. Já na parte do Redux, inicialmente a implementação foi feita Thunk e após, comentada, e implementado em Saga para fins didáticos.
+
+## Technologies
+
+- ReactJs
+- Sass
+- MirageJs
+- Redux-Thunk
+- Redux-Saga
+
+## Features
+
+- Visualizar filmes cadastrados
+- Editar filmes cadastrados
+- Inserir filmes na listagem
+- Excluir filmes da listagem
+
+## Prototypes
+
+## Getting started
+
+**Frontend**
+
+- npm install
+- npm start
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { criarServidor } from "./services/mirage-server";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import routes from "./routes";
+import "./i18n";
+
+const ambiente = process.env.NODE_ENV;
+if (ambiente !== "production") {
+  criarServidor({ environment: ambiente });
+}
+
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      {routes.map((route, index) => (
+        <Route
+          key={index}
+          path={route.path}
+          component={route.component}
+          exact={route.exact}
+        />
+      ))}
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
